877d8040689a92282b386779d8351954417fea2e
Tristan Greffe <tristan.greffe@hotmail.com>
2023-04-27 22:26:07 +0200

    wip: initialize registration pages  #2

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,18 @@ const routes = [
     path: '/',
     component: () => import('pages/IndexPage.vue'),
   },
-
+  {
+    path: '/login',
+    component: () => import('pages/LoginPage.vue'),
+  },
+  {
+    path: '/sign-up',
+    component: () => import('pages/SignUpPage.vue'),
+  },
+  {
+    path: '/chat',
+    component: () => import('pages/ChatPage.vue'),
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
