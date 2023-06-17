0d7168eded2389bc3a05e18b29f4b02e0f1b2a6a
Tristan Greffe <tristan.greffe@hotmail.com>
2023-06-17 22:42:56 +0200

    refactor: refactoring of internationalization initialization logic to a dedicated file

--- src/boot/utils.js
+++ src/boot/utils.js
@@ -1,7 +1,11 @@
+import { initializeI18n } from '../i18n'
 import Header from '../components/Header.vue'
 import Footer from '../components/Footer.vue'
 
 export default async ({ app }) => {
+  // Initializes i18n
+  initializeI18n(app)
+
   // Register global components
   app.component('Header', Header)
   app.component('Footer', Footer)
