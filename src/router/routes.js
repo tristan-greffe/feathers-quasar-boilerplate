cbad302820c600e53b5b88904608898fcb4fad17
Tristan Greffe <tristan.greffe@hotmail.com>
2023-06-22 23:54:48 +0200

    chore: add new routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,27 +1,22 @@
 
 const routes = [
-  {
-    path: '/',
-    component: () => import('pages/IndexPage.vue'),
+  { path: '/', component: () => import('pages/IndexPage.vue') },
+  { path: '/login', component: () => import('pages/LoginPage.vue') },
+  { path: '/sign-up', component: () => import('pages/SignUpPage.vue') },
+  { path: '/admin',
+    children: [
+      { path: 'users', component: () => import('pages/admin/UsersPage.vue') }
+    ]
   },
-  {
-    path: '/login',
-    component: () => import('pages/LoginPage.vue'),
-  },
-  {
-    path: '/sign-up',
-    component: () => import('pages/SignUpPage.vue'),
-  },
-  {
-    path: '/chat',
-    component: () => import('pages/ChatPage.vue'),
+  { path: '/dashboard',
+    children: [
+      { path: 'chat', component: () => import('pages/dashboard/ChatPage.vue') },
+      { path: 'settings', component: () => import('pages/dashboard/SettingsPage.vue') }
+    ],
   },
   // Always leave this as last one,
   // but you can also remove it
-  {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
+  { path: '/:catchAll(.*)*', component: () => import('pages/404Page.vue') }
 ]
 
 export default routes
