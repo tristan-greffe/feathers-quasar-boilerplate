b582e15b33408203f7c9dbea8024792106d8395c
Tristan Greffe <tristan.greffe@hotmail.com>
2023-06-26 23:24:25 +0200

    refactor: updated i18n initialization & added method for key translation

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -21,26 +21,35 @@ function getLocale () {
   return Quasar.lang.isoName
 }
 
-export async function initializeI18n (app) {
-  // Define the locale to be used
-  const locale = getLocale()
-  // Define messages
-  const messages = {
-    'en-US': en,
-    'fr': fr
-  }
-  // Install Quasar langage pack
-  try {
-    const langagePack = await import(`quasar/lang/${locale}.js`)
-    if (langagePack) Quasar.lang.set(langagePack.default)
-  } catch (error) {
-    logger.error(error)
-  }
-  // Create i18n instance
-  const i18n = createI18n({
-    locale,
-    messages,
-    globalInjection: true
-  })
-  app.use(i18n)
+export const i18n = {
+  async initialize (app) {
+    // Define the locale to be used
+    const locale = getLocale()
+    // Define messages
+    const messages = {
+      'en-US': en,
+      'fr': fr
+    }
+    // Install Quasar langage pack
+    try {
+      const langagePack = await import(`quasar/lang/${locale}.js`)
+      if (langagePack) Quasar.lang.set(langagePack.default)
+    } catch (error) {
+      logger.error(error)
+    }
+    // Create i18n instance
+    this.i18n = createI18n({
+      locale,
+      messages,
+      globalInjection: true
+    })
+    app.use(this.i18n)
+  },
+  t (key, params) {
+    if (!this.i18n) {
+      logger.error('i18n instance is not existing. Did you initialize it ?')
+      return key
+    }
+    return this.i18n.global.t(key, params)
+  },
 }
