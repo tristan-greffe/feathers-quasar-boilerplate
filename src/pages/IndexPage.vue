f6d77381430b9e5e65e97870527c754f0f885c50
Tristan Greffe <tristan.greffe@hotmail.com>
2023-07-01 21:55:46 +0200

    feat: implementation of an interactive map with leaflet

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,16 +1,32 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <Header />
-    <q-page-container>
-      <q-page class="flex flex-center">
-      </q-page>
-    </q-page-container>
-    <Footer />
-  </q-layout>
+  <q-page-container>
+    <q-page class="relative-position">
+      <Sidebar />
+      <div id="map" ref="mapRef"></div>
+    </q-page>
+  </q-page-container>
+</q-layout>
+
 </template>
 
-<script>
-export default {
-  name: 'IndexPage',
-}
+<script setup>
+import { ref, onMounted } from 'vue'
+import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
+import Sidebar from '../components/Sidebar'
+// Data
+const mapRef = ref(null)
+const markerRef = ref(null)
+
+// onMounted
+onMounted(() => {
+  const map = L.map(mapRef.value).setView([46.05, 11.05], 5)
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map)
+  
+  markerRef.value = L.marker([51.5, -0.09]).addTo(map).bindPopup('hello')
+})
 </script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -8,12 +8,6 @@ const routes = [
       { path: 'users', component: () => import('pages/admin/UsersPage.vue') }
     ]
   },
-  { path: '/dashboard',
-    children: [
-      { path: 'chat', component: () => import('pages/dashboard/ChatPage.vue') },
-      { path: 'settings', component: () => import('pages/dashboard/SettingsPage.vue') }
-    ],
-  },
   // Always leave this as last one,
   // but you can also remove it
   { path: '/:catchAll(.*)*', component: () => import('pages/404Page.vue') }
