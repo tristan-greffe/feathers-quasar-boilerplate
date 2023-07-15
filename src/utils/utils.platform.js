0910c4b05810fefdb3c16bc6029bea647147e9b4
Tristan Greffe <tristan.greffe@hotmail.com>
2023-07-15 22:25:53 +0200

    feat: implement utils.platform.js

---
+++ src/utils/utils.platform.js
@@ -0,0 +1,11 @@
+import { Platform } from 'quasar'
+
+export function getPlatform () {
+  return Object.assign(Platform.is, {
+    touch: Platform.has.touch,
+    storage: Platform.has.webStorage,
+    iframe: Platform.within.frame,
+    agent: Platform.userAgent
+  })
+}
+
