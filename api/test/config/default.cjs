6e395a338234558eeb285242faa1277761c2b407
Tristan Greffe <tristan.greffe@hotmail.com>
2023-08-17 22:30:51 +0200

    test: implement tests

---
+++ api/test/config/default.cjs
@@ -0,0 +1,63 @@
+var path = require('path')
+var fs = require('fs')
+var containerized = require('containerized')()
+
+const serverPort = process.env.PORT || 8081
+const API_PREFIX = '/api'
+
+module.exports = {
+  apiPath: API_PREFIX,
+  paginate: { default: 10, max: 50 },
+  mongodb: containerized ? 'mongodb://mongodb:27017/feathers-quasar-boilerplate' : 'mongodb://127.0.0.1:27017/feathers-quasar-boilerplate',
+  host: process.env.HOSTNAME || 'localhost',
+  port: serverPort,
+  distPath: fs.existsSync(path.join(__dirname, '../../dist/pwa')) ? path.join(__dirname, '../../dist/pwa') : path.join(__dirname, '../../dist/spa'),
+  passwordPolicy: {
+    minLength: 8,
+    maxLength: 128,
+    uppercase: true,
+    lowercase: true,
+    digits: true,
+    symbols: true,
+    prohibited: fs.readFileSync(path.join(__dirname, '10k_most_common_passwords.txt')).toString().split('\n')
+  },
+  mailer: {
+    service: 'gmail',
+    auth: {
+      user: process.env.GOOGLE_MAIL_USER,
+      pass: process.env.GOOGLE_MAIL_PASWORD
+    },
+    templateDir: path.join(__dirname, 'email-templates')
+  },
+  proxyTable: {},
+  public: './public',
+  authentication: {
+    secret: process.env.APP_SECRET || 'my secret',
+    service: API_PREFIX + '/users',
+    path: API_PREFIX + '/authentication',
+    entity: 'user',
+    authStrategies: [
+      'jwt',
+      'local',
+      'google'
+    ],
+    local: {
+      usernameField: 'email',
+      passwordField: 'password'
+    },
+    jwtOptions: {
+      header: {
+        typ: 'access' // See https://tools.ietf.org/html/rfc7519#section-5.1
+      },
+      audience: 'https://tristan-code.xyz',
+      algorithm: 'HS256', // See https://github.com/auth0/node-jsonwebtoken#jwtsignpayload-secretorprivatekey-options-callback
+      expiresIn: '1d'
+    },
+    oauth: {
+      google: {
+        key: process.env.GOOGLE_CLIENT_ID,
+        secret: process.env.GOOGLE_CLIENT_SECRET,
+      },
+    }
+  }
+}
---
+++ api/test/index.test.js
@@ -0,0 +1,172 @@
+import { feathers } from '@feathersjs/feathers'
+import express, { rest } from '@feathersjs/express'
+import configuration from '@feathersjs/configuration'
+import _ from 'lodash'
+import { mongodb } from '../src/mongodb.js'
+import { authentication } from '../src/authentication.js'
+import { services } from '../src/services/index.js'
+
+import assert from 'assert'
+import request from 'superagent'
+import chai from 'chai'
+import chailint from 'chai-lint'
+
+const { util, expect } = chai
+
+describe('core:services', () => {
+  let app, server, port, userService, baseUrl, accessToken, userObject
+
+  before(async () => {
+    chailint(chai, util)
+    app = express(feathers())
+    app.configure(configuration())
+    const bodyParserConfig = app.get('bodyParser')
+    app.use(express.json(_.get(bodyParserConfig, 'json')))
+    app.configure(rest())
+    app.configure(mongodb)
+    app.configure(authentication)
+    port = app.get('port')
+    baseUrl = `http://localhost:${port}${app.get('apiPath')}`
+  })
+  it('registers the services', async () => {
+    await app.configure(services)
+    userService = app.service('api/users')
+    expect(userService).toExist()
+    expect(app.service('api/authentication')).toExist()
+    expect(app.service('api/mailer')).toExist()
+    expect(app.service('api/account')).toExist()
+    // Now app is configured launch the server
+    server = await app.listen(port)
+    await new Promise(resolve => server.once('listening', () => resolve()))
+  })
+  // Let enough time to process
+    .timeout(10000)
+
+  it('cannot create a user with a weak password', async () => {
+    await assert.rejects(() => userService.create({
+      email: 'test@test.org',
+      password: 'weak;',
+      name: 'test-user'
+    }), error => {
+      expect(error).toExist()
+      expect(error.name).to.equal('BadRequest')
+      expect(error.data.translationKey).to.deep.equal('WEAK_PASSWORD_MIN')
+      return true
+    })
+
+    await assert.rejects(() => userService.create({
+      email: 'test@test.org',
+      password: '12345678',
+      name: 'test-user'
+    }), error => {
+      expect(error).toExist()
+      expect(error.name).to.equal('BadRequest')
+      expect(error.data.translationKey).to.deep.equal('WEAK_PASSWORD_UPPERCASE')
+      return true
+    })
+
+    await assert.rejects(() => userService.create({
+      email: 'test@test.org',
+      password: 'PASSWORDTOTO',
+      name: 'test-user'
+    }), error => {
+      expect(error).toExist()
+      expect(error.name).to.equal('BadRequest')
+      expect(error.data.translationKey).to.deep.equal('WEAK_PASSWORD_LOWERCASE')
+      return true
+    })
+
+    await assert.rejects(() => userService.create({
+      email: 'test@test.org',
+      password: 'PasswordToto',
+      name: 'test-user'
+    }), error => {
+      expect(error).toExist()
+      expect(error.name).to.equal('BadRequest')
+      expect(error.data.translationKey).to.deep.equal('WEAK_PASSWORD_DIGITS')
+      return true
+    })
+
+    await assert.rejects(() => userService.create({
+      email: 'test@test.org',
+      password: 'PasswordToto1',
+      name: 'test-user'
+    }), error => {
+      expect(error).toExist()
+      expect(error.name).to.equal('BadRequest')
+      expect(error.data.translationKey).to.deep.equal('WEAK_PASSWORD_SYMBOLS')
+      return true
+    })
+  })
+  // Let enough time to process
+    .timeout(20000)
+
+  it('creates a user', () => {
+    return userService.create({
+      email: 'test@test.org',
+      password: 'Pass;word1',
+      name: 'test-user'
+    }).then(user => {
+      userObject = user
+      return userService.find({ query: { email: 'test@test.org' } })
+    })
+      .then(users => {
+        expect(users.data.length > 0).beTrue()
+        expect(users.data[0].name).toExist()
+        expect(users.data[0].name).to.equal('test-user')
+        expect(users.data[0].isVerified).to.equal(false)
+        expect(users.data[0].email).toExist()
+        expect(users.data[0].email).to.equal('test@test.org')
+      })
+  })
+  // Let enough time to process
+    .timeout(10000)
+
+  it('authenticates a user', () => {
+    return request
+      .post(`${baseUrl}/authentication`)
+      .send({ email: 'test@test.org', password: 'Pass;word1', strategy: 'local' })
+      .then(response => {
+        accessToken = response.body.accessToken
+        expect(accessToken).toExist()
+      })
+  })
+  // Let enough time to process
+    .timeout(10000)
+
+  it('authenticated user can access services', () => {
+    return userService.find({ query: {}, params: { user: userObject } })
+      .then(users => {
+        expect(users.data.length === 1).beTrue()
+      })
+  })
+  // Let enough time to process
+    .timeout(10000)
+
+  it('unauthenticates a user', () => {
+    return request
+      .del(`${baseUrl}/authentication`)
+      .set('Content-Type', 'application/json')
+      .set('Authorization', accessToken)
+      .then(response => {
+        expect(response.status).to.equal(200)
+      })
+  })
+
+  it('removes a user', () => {
+    return userService.remove(userObject._id, { user: userObject })
+      .then(user => {
+        return userService.find({ query: { name: 'test-user' } })
+      })
+      .then(users => {
+        expect(users.data.length === 0).beTrue()
+      })
+  })
+  // Let enough time to process
+    .timeout(5000)
+
+  // Cleanup
+  after(async () => {
+    if (server) await server.close()
+  })
+})
